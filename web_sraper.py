ce3e4e50890afbe136d70ec115d275c0646987f9
Bani Somo <banisomo2003@gmail.com>
2023-12-29 18:12:38 -0500

    Add TTS

--- web_sraper.py
+++ web_sraper.py
@@ -1,15 +1,19 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+from gtts import gTTS
+from pydub import AudioSegment
 
-url = "https://www.reddit.com/r/AmItheAsshole/top/"
+
+url = "https://www.reddit.com/r/AmItheAsshole/top/"#'https://www.reddit.com/r/UnethicalLifeProTips/top/'#'https://www.reddit.com/r/Showerthoughts/top/'#
+outputfile = 'speech.mp3'
 
 
-try:
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-except:
-    exit(-1)
+MAX_POST_LENGTH = 1250 # to keep video length under 1 minute
+
+
+page = urlopen(url)
+html = page.read().decode("utf-8")
 
 
 soup = BeautifulSoup(html, "html.parser")
@@ -20,16 +24,26 @@ def post_selector(tag):
 
 
 posts = soup.find_all(post_selector)
-link_with_post_title = posts[0].find_all('a')[0]
-link_with_post_body = posts[0].find_all('a')[4]
-
+link_with_post_title = posts[1].find_all('a')[0]
+link_with_post_body = posts[1].find_all('a')[4]
 
 title = link_with_post_title.attrs.get('aria-label')
 
 
-text = ""
+body = ""
 for paragraph in link_with_post_body.find_all('p'):
-    text += paragraph.getText()
+    body += paragraph.getText()
 
-print(title + text)
-print(len(title + text))
+
+text = title + body
+text = text.replace('AITA', 'Am I the A-hole')
+text = text.replace('ULPT', 'Unethical life pro tip')
+text = text.replace('\n', '')
+tts = gTTS(text=text, lang='en')
+tts.save(outputfile)
+print(len(text))
+
+
+audio = AudioSegment.from_file(outputfile, format="mp3")
+faster_audio = audio.speedup(playback_speed=1.5)
+faster_audio.export(outputfile, format="mp3") 
